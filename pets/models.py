0575e89b75ca23355681e726612766f8e8d1dc92
gustavodm <gusttavomota@gmail.com>
2021-07-26 19:25:47 -0300

    feat: add Animal, Characteristic and Group models

---
+++ pets/models.py
@@ -0,0 +1,21 @@
+from django.db.models import Model, CharField, FloatField
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+
+
+class Group(Model):
+    name = CharField(max_length=100)
+    scientific_name = CharField(max_length=100)
+
+
+class Characteristic(Model):
+    name = CharField(max_length=255)
+
+
+class Animal(Model):
+    name = CharField(max_length=100)
+    age = FloatField()
+    weight = FloatField()
+    sex = CharField(max_length=30)
+    group_id = ForeignKey(Group, on_delete=CASCADE)
+    characteristics = ManyToManyField(Characteristic)
